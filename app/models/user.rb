2e76a46b7f71db5e7340511345f05867331c8758
marlow <moreno.omar89@gmail.com>
2010-06-04 11:27:02 -0700

    adding passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor	:password
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -7,5 +8,11 @@ class User < ActiveRecord::Base
 	validates_length_of	:name, :maximum => 50
 	validates_format_of	:email, :with => EmailRegex
 	validates_uniqueness_of	:email, :case_sensitive => false
+	
+	#automatically create the virtual attriubte 'password_confirmation'.
+	validates_confirmation_of :password
 
+	#password validations
+	validates_presence_of	:password
+	validates_length_of	:password, :within => 6..40
 end
