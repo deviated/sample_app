749d11d0afbb8980ab8ef8cf62cc32cb91c7dd5c
marlow <moreno.omar89@gmail.com>
2010-06-08 11:50:39 -0700

    Get Factory Girl

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe UsersController do
 	integrate_views
 
+  describe "Get 'show'" do
+  	
+	before(:each) do
+		@user = Factory(:user)
+		User.stub!(:find, @user.id).and_return(@user)
+	end
+
+	it "should be successful" do
+		get :show, :id => @user
+		response.should be_success
+	end
+  end
+
   #Delete these examples and add some real ones
   it "should use UsersController" do
     controller.should be_an_instance_of(UsersController)
