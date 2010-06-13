d0b7bf78d2c516b6c6b69c3b6f7c5b99ec6d42c7
marlow <moreno.omar89@gmail.com>
2010-06-12 18:40:32 -0700

    before paignate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def new
   	@title = "Sign up"
 	@user = User.new
@@ -21,5 +24,35 @@ class UsersController < ApplicationController
 		#@user.password.reset
 	end
   end
+
+  def edit
+	@title = "Edit user"
+  end
+
+  def update
+	if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated."
+			redirect_to @user
+	else
+		@title = "Edit user"
+		render 'edit'
+	end
+  end
+
+  def index
+  	@title = "All users"
+	@users = User.all
+  end
+
+  private
+
+  	def authenticate
+		deny_access unless signed_in?
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
 
 end
